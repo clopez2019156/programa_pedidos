9fadf737a80ac07c2769aeb864af1c0d051d5fbf
Cesar <clopez-2019156@kinal.edu.gt>
2021-08-25 11:17:08 -0600

    subida del proyecto

---
+++ inventarios_php/bd/inicia_conexion.php
@@ -0,0 +1,8 @@
+<?php 
+	$con = mysqli_connect("192.168.0.7", "admin", "");
+	if (!$con) {
+		die('Could not connect: '. mysqli_connect_error());
+	}
+	mysqli_select_db($con, "dbinventarios");
+	
+?>
